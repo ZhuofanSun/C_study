32a9cc30dad4645dbb192cd625702f84682f1f90
Zhuofan Sun <zhuofan3@ualberta.ca>
2023-02-27 23:33:37 -0700

    Lab7 Part2

--- 201/201_tests/lab_test/lab7/get_extension.c
+++ 201/201_tests/lab_test/lab7/get_extension.c
@@ -1,11 +1,31 @@
 #include <stdio.h>
-
+#include <string.h>
 #define MAX_SIZE 20
 
 int get_extension(const char *file_name);
-
 int main(int argc, char* argv[]){
-	
+
 	//simplifying things and not doing any argument validation
     printf("The index value of the extension of the input file is: %d\n", get_extension(argv[1]));
+}
+
+
+
+int get_extension(const char *file_name) {
+    static const char* extensions[] = {".txt", ".out", ".bkp", ".dot", ".tx"};
+    int name_len = strlen(file_name), extension_len;
+    char * substring;  // pointer of the expected extension
+
+    for (int i = 0; i < 5; i++) {
+        extension_len = strlen(extensions[i]);
+        if (name_len < extension_len){
+            continue;
+        }
+
+        substring = file_name + name_len - extension_len;
+        if (strcmp(substring, extensions[i]) == 0) {  // true expected extension
+            return i;
+        }
+    }
+    return -1;
 }
