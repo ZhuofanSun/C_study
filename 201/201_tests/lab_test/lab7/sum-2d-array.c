25ccd7c6549dacf82c4ec40e120652d1036ed023
Zhuofan Sun <zhuofan3@ualberta.ca>
2023-03-08 22:07:44 -0700

    commit at 2023年 3月 8日 星期三 22时07分44秒 MST

--- 201/201_tests/lab_test/lab7/sum-2d-array.c
+++ 201/201_tests/lab_test/lab7/sum-2d-array.c
@@ -3,22 +3,20 @@
 #define LEN 10
 
 int sum_two_dimensional_array(int a[][LEN], int n){
-    int i, j, k, sum = 0, total_num = n*LEN;  // no. of number, outer pointer, inner pointer
+    int i, j, k, sum = 0, total_num = n*LEN;
     for (i = 0; i < total_num; ++i) {
         j = i / LEN;  // [0, inf] add by one every LEN
         k = i % LEN;  // [0, LEN-1]
-        sum += *(*(a + j) + k);  // 2 pointers int 2 dimensions
+        sum += *(*(a + j) + k);
     }
     return sum;
 }
 
 int main(){
-	//int numbers[2][LEN] = {{1,2,3,4,5,6,7,8,9,10},
-	//			{2,3,4,5,6,7,8,9,10,11}};
+	int numbers[2][LEN] = {{1,2,3,4,5,6,7,8,9,10},
+			{2,3,4,5,6,7,8,9,10,11}};
 
-	//printf("The sum of all elements of the array is: %d\n", sum_two_dimensional_array(numbers, 2));
-    int a = 0, b = 1;
-    a = &b;
+	printf("The sum of all elements of the array is: %d\n", sum_two_dimensional_array(numbers, 2));
 
 	return 0;
 }
